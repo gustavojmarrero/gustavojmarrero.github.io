ea170feeca6b0dbdda11b91a87ed0f9e335f2de3
Gustavo <gustavojmarrero>
2023-07-28 13:59:09 -0500

    cambios menores

--- src/js/script.js
+++ src/js/script.js
@@ -1,57 +1,5 @@
-// Index
-
-window.onload = function () {
-  // Obtén la ruta de la página actual
-  const path = window.location.pathname;
-
-  // Extrae el nombre de la página de la ruta
-  const page = path.split("/").pop();
-
-  // Solo ejecuta el código si la página es index.html
-  if (page === "index.html" || page === "") {
-    FNSKU.focus();
-  }
-};
 
 
-function getDataByFNSKU() {
-    var FNSKU = document.getElementById("FNSKU").value;
-    document.getElementById("FNSKU").value = "";
-    document.getElementById("FNSKUdisplay").innerText = FNSKU;
-
-    // Replace with the URL of your web app
-    var apiEndpoint =
-      "https://script.google.com/macros/s/AKfycbyaWUL6PDWQkyiSC1-oubkhJLfoKzNwjUNP07zVmNDtPIx24-d5t44KCdwUc_6yiXsRuQ/exec?fnsku=" +
-      FNSKU;
-
-    document.getElementById("loader").classList.remove("d-none");
-    fetch(apiEndpoint)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Success:", data);
-        document.getElementById("apiTitle").innerText =
-          data.resultado.title;
-        document.getElementById("apiImage").src = data.resultado.image;
-        document.getElementById("ASINdisplay").innerText = data.resultado
-          .asin
-          ? data.resultado.asin
-          : "";
-        if (data.resultado.pao) {
-          document.getElementById("PAOdisplay").innerText =
-            data.resultado.pao;
-        } else {
-          document.getElementById("PAOdisplay").innerText =
-            "Falta definir Caducidad";
-        }
-        document.getElementById("loader").classList.add("d-none");
-        document.getElementById("FNSKU").focus(); // Focus on the input after fetching data
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        document.getElementById("loader").classList.add("d-none");
-        document.getElementById("FNSKU").focus(); // Focus on the input after catching an error
-      });
-  }
 
 
 
