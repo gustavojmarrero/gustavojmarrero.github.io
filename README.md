9bcaafb0ef2e9d2874f564068e5ec128e3b4407e
Gustavo <gustavojmarrero>
2023-08-03 11:03:04 -0500

    Cambia la pagina FNSKU por la de producto, ahora se puede buscar por UPC, ASIN o FNSKU

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # gustavojmarrero.github.io
+
+
+## Futuras Mejoras
+
+Agregar al buscador por FNSKU buscar por UPC
+
+
+
+api pedido https://script.google.com/u/0/home/projects/1bJ-Do4bp7Z2QdBfimPyb6Q00zevHqVg6oNC8CpIVV8CP_dzLCe3g_hox/edit?pli=1
+api pedidos https://script.google.com/u/0/home/projects/1vAWsdAwDRp58EGrsvBYOsfcsFuYn-1i976Zz-oKhQbFkVVdOyjmKLfeE/edit
--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,14 @@
-window.onload = () => document.getElementById('FNSKU').focus();
+window.onload = () => document.getElementById("PRODUCT").focus();
 
 const loader = document.getElementById("loader");
 
 
-const getDataByFNSKU = () => {
-  const FNSKU = document.getElementById("FNSKU").value;
-  document.getElementById("FNSKU").value = "";
-  document.getElementById("FNSKUdisplay").innerText = FNSKU;
+const getDataByCode = () => {
+  const PRODUCT = document.getElementById("PRODUCT").value;
+  document.getElementById("PRODUCT").value = "";
+  //document.getElementById("CODEdisplay").innerText = CODE;
 
-  const apiEndpoint = `https://script.google.com/macros/s/AKfycbyaWUL6PDWQkyiSC1-oubkhJLfoKzNwjUNP07zVmNDtPIx24-d5t44KCdwUc_6yiXsRuQ/exec?fnsku=${FNSKU}`;
+  const apiEndpoint = `https://script.google.com/macros/s/AKfycbyaWUL6PDWQkyiSC1-oubkhJLfoKzNwjUNP07zVmNDtPIx24-d5t44KCdwUc_6yiXsRuQ/exec?code=${PRODUCT}`;
 
   showLoader();
 
@@ -17,16 +17,18 @@ const getDataByFNSKU = () => {
       console.log("Success:", data);
       document.getElementById("apiTitle").innerText = data.resultado.title;
       document.getElementById("apiImage").src = data.resultado.image;
+      document.getElementById("FNSKUdisplay").innerText = data.resultado.fnsku ? data.resultado.fnsku : "";
       document.getElementById("ASINdisplay").innerText = data.resultado.asin ? data.resultado.asin : "";
+      document.getElementById("UPCdisplay").innerText = data.resultado.upc ? data.resultado.upc : "";
       
       document.getElementById("PAOdisplay").innerText = data.resultado.pao ? data.resultado.pao : "Falta definir Caducidad";
       
       hideLoader();
-      document.getElementById("FNSKU").focus();
+      document.getElementById("PRODUCT").focus();
     })
     .catch(error => {
       console.error("Error:", error);
       hideLoader();
-      document.getElementById("FNSKU").focus();
+      document.getElementById("PRODUCT").focus();
     });
 }
